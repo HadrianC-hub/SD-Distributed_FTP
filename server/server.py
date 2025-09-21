fde92e892989ca8f962b69f77cd457de8b44bee0
Adrián Hernández Castellanos <hadrianc02@gmail.com>
2025-09-21 11:21:26 -0400

    feat: configuración inicial y constantes globales

--- server/server.py
+++ server/server.py
@@ -38,3 +38,30 @@ PORT = 21
 failed_attempts = {}
 
 class Session:
+    def __init__(self, client_socket, client_addr):
+        self.client_socket = client_socket
+        self.client_addr = client_addr
+        self.client_ip = client_addr[0]
+        self.authenticated = False
+        self.username = None
+        base_root = os.path.abspath(SERVER_ROOT)
+        os.makedirs(base_root, exist_ok=True)
+        self.root_dir = base_root
+        self.current_dir = self.root_dir
+        self.type = 'A'   # ASCII por defecto
+        self.mode = 'S'   # Stream por defecto
+        self.stru = 'F'   # File por defecto
+        self.passive_listener = None
+        self.data_socket = None
+        self.rename_from = None
+        self.last_activity = time.time()
+
+# -------------------- Gestión de usuarios --------------------
+
+# --- Rutas base multiplataforma ---
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))   # carpeta donde está server.py
+SERVER_ROOT = BASE_DIR                                  # raíz real del servidor
+USERS_FILE = os.path.normpath(os.path.join(SERVER_ROOT, "users.json"))
+
+def hash_password(password: str) -> str:
+    """Devuelve hash PBKDF2 seguro."""
