ecdcc3f7ff144ab2cac10cfa807fe5bd376e2294
LauraMarby <lauramartir2@gmail.com>
2025-09-28 14:19:48 -0400

    init: inicialización del script para interfaz grafica en client

---
+++ client/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import os
+import sys
+import io
+import time
+import re
+import time
+import client as client
+
+st.set_page_config(page_title="Cliente FTP", page_icon="📂", layout="wide")
